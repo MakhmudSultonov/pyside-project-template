d8ae1e828ab90a02a6b1e32eae4dd48f1005d49c
MakhmudSultonov <MakhmudSultonov@gmail.com>
2025-05-12 17:52:27 +0500

    first commit

---
+++ run.py
@@ -0,0 +1,13 @@
+import os
+import shutil
+import sys
+
+# Virtual muhitdagi Python interpretatorini aniqlash
+venv_python = os.path.join(os.path.dirname(os.path.abspath(__file__)), "env", "Scripts", "python.exe")
+print(venv_python)
+
+print("QML resurslarini yangilash...")
+os.system("pyside6-rcc qml.qrc -o py/rc_qml.py")
+
+print("\nDasturni ishga tushirish...")
+os.system(f'"{venv_python}" py/main.py') 
